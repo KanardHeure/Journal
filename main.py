671d5b17fb86bb25e5090a02d4cc35294527b67a
Kanardheure <brehier.colin@gmail.com>
2019-03-27 11:57:41 +0100

    Réorganisation du code

--- main.py
+++ main.py
@@ -13,52 +13,13 @@
 # Etape 6 : Réorganisation du code sur plusieurs fichier .py
 # Etape 7 : Rajouter des questions et utiliser un peu d'aléatoire (tout en laissant des questions obligatoire)
 
-
-from os import path
-from os import getcwd
-from os import mkdir
-from os import chdir
-from os import system
+### IMPORT ###
 import sys
-import datetime
+import create_folder_file as cff
+import List_of_question as Loq
+##############
 
-curdir = path.dirname(__file__)
-if len(curdir) == 0: curdir = getcwd()
-
-if not path.exists(path.join(curdir,'Journal de bord')):
-    mkdir(path.join(curdir,'Journal de bord'))
-chdir(curdir + "/Journal de bord")
-
-message = ["Bonjour, qu'as-tu fait aujourd'hui?", "Et comment c'est passé ta journée ?", "Sur une échelle de 1 à 10, à quel point tu t'es senti dépressif", "As-tu pleurer aujourd'hui ?"]
-reponse = []
-i = 0
-date = datetime.date.today()
-
-for item in message:
-    system("cls")
-    system("clear")
-    print(item)
-    reponse.append(input("\n"))
-
-
-
-liste_mois = ["Janvier", "Fevrier", "Mars", "Avril", "Mai", "Juin", "Juillet", "Aout", "Septembre", "Octobre", "Novembre", "Decembre"]
-liste_mois_chiffre = [1,2,3,4,5,6,7,8,9,10,11,12]
-
-
-
-for item in liste_mois_chiffre:
-    if item == (date.month):
-        nom_mois = liste_mois[(item-1)]
-
-journal = open("Journal de " + nom_mois + " " + str(date.year) +".txt", 'a')
-journal.write("Journal du " + str(date.day) + " " + nom_mois + " " + str(date.year) + "\n")
-journal.write("\n")
-
-
-for item in message:
-    journal.write(item)
-    journal.write("\n\t" + reponse[i] + "\n\n")
-    i+= 1
-journal.close()
+cff.create_directory()
+message, reponse = Loq.question()
+cff.create_journal_file(message, reponse)
 sys.exit(0)
